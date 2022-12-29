a28ab8367cf4f566f90d44429ec0e1f4f091d64e
Jules Maestro <jlsodai@gmail.com>
2022-12-29 23:20:43 +0000

    add filter buttons

--- sections/ContentSection.jsx
+++ sections/ContentSection.jsx
@@ -1,7 +1,27 @@
+import FilterBtn from "../components/FilterBtn";
 const ContentSection = () => {
 	return (
-		<div className="px-8">
-			<div className="">filter chips</div>
+		<div className="px-8 w-full">
+			<div className="flex gap-4 flex-nowrap overflow-x-auto filterSection">
+				<FilterBtn title="All" active />
+				<FilterBtn title="Computer programming" />
+				<FilterBtn title="Music" />
+				<FilterBtn title="Mixes" />
+				<FilterBtn title="Live" />
+				<FilterBtn title="Nollywood" />
+				<FilterBtn title="Premier League" />
+				<FilterBtn title="Dramedy" />
+				<FilterBtn title="Podcasts" />
+				<FilterBtn title="Comedy" />
+				<FilterBtn title="Russian Armed Forces" />
+				<FilterBtn title="Contemporary Worship Music" />
+				<FilterBtn title="Children's Music" />
+				<FilterBtn title="Stages" />
+				<FilterBtn title="Pop Music" />
+				<FilterBtn title="Recently Uploaded" />
+				<FilterBtn title="Watched" />
+				<FilterBtn title="New to you" />
+			</div>
 			<div className="grid grid-cols-4">
 				<div>Video Card</div>
 				<div>Video Card</div>
