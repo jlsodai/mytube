0642ea0f2bc565bea5feac88b9aea339012270a8
Jules Maestro <jlsodai@gmail.com>
2022-12-29 16:20:43 +0000

    configure tailwind, make default font roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{html,js,jsx}',
-    './components/**/*.{html,js,ts,jsx,tsx}',
-    './sections/**/*.{html,js,jsx}',
-    './styles/**/*.{js,jsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./app/**/*.{html,js,jsx}",
+		"./components/**/*.{html,js,ts,jsx,tsx}",
+		"./sections/**/*.{html,js,jsx}",
+		"./styles/**/*.{js,jsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Roboto", "Arial", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
