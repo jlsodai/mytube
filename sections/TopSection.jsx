3c71f0e4590314760959311d4837b687f899ccaf
Jules Maestro <jlsodai@gmail.com>
2022-12-29 17:27:55 +0000

    use new Icon component

--- sections/TopSection.jsx
+++ sections/TopSection.jsx
@@ -6,9 +6,7 @@ const TopSection = () => {
 	return (
 		<div className="flex justify-between pl-4 pr-8 py-2 items-center">
 			<div className="flex items-center">
-				<div className="p-3">
-					<Icon src="/svg/hamburger.svg" />
-				</div>
+				<Icon src="/svg/hamburger.svg" />
 				<div className="px-4">
 					<Image src="/svg/logo.svg" alt="" width="95" height="24" />
 				</div>
