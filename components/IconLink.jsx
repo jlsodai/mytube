af6241cae44c224328341d13385cdbefa4d3b41f
Jules Maestro <jlsodai@gmail.com>
2022-12-29 17:27:32 +0000

    add IconLink component

---
+++ components/IconLink.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+const IconLink = ({ src, href, title }) => {
+	return (
+		<Link className="px-3 py-2 flex items-center" href={href ?? "#"}>
+			<Image src={src} alt="" width="24" height="24" />
+			<div className="ml-7">{title}</div>
+		</Link>
+	);
+};
+export default IconLink;
