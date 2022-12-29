cb59499937415240c5100b890aa09ea50f0d85b5
Jules Maestro <jlsodai@gmail.com>
2022-12-29 18:21:42 +0000

    add image domains

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-}
+	// reactStrictMode: true,
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		domains: ["images.unsplash.com", "yt3.ggpht.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
