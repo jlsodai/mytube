0cc0fbcf2b53bcf295e8f91bd56c1c58ec0a50b9
Jules Maestro <jlsodai@gmail.com>
2022-12-29 16:42:35 +0000

    fix padding add hamburger and logo

--- sections/TopSection.jsx
+++ sections/TopSection.jsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 const TopSection = () => {
 	return (
-		<div className="flex justify-between px-8 py-4">
-			<div className="flex">
-				<div className="">Ham </div>
-				<div className="">Logo Section</div>
+		<div className="flex justify-between pl-4 pr-8 py-2 items-center">
+			<div className="flex items-center">
+				<div className="p-3">
+					<Image
+						src="/svg/hamburger.svg"
+						alt=""
+						width="24"
+						height="24"
+					/>
+				</div>
+				<div className="px-4">
+					<Image src="/svg/logo.svg" alt="" width="95" height="24" />
+				</div>
 			</div>
 			<div className="">Search Area</div>
 			<div className="">Profile Icons section</div>
