48952e5478415283194eabba65d6d53f77f9d5b2
Jules Maestro <jlsodai@gmail.com>
2022-12-29 17:52:59 +0000

    add active prop to IconLink

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-const IconLink = ({ src, href, title }) => {
+const IconLink = ({ src, href, title, active }) => {
 	return (
-		<Link className="px-3 py-2 flex items-center" href={href ?? "#"}>
+		<Link
+			className={`${
+				active ? "bg-gray-100 rounded-md" : ""
+			} px-3 py-2 flex items-center`}
+			href={href ?? "#"}
+		>
 			<Image src={src} alt="" width="24" height="24" />
 			<div className="ml-7">{title}</div>
 		</Link>
