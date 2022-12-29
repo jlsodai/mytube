d3cb3e0f514f1ff094f82bbec832ff013ad9df35
Jules Maestro <jlsodai@gmail.com>
2022-12-29 16:42:51 +0000

    fix padding

--- sections/SidebarSection.jsx
+++ sections/SidebarSection.jsx
@@ -3,19 +3,19 @@ import FooterSection from "./FooterSection";
 const SidebarSection = () => {
 	return (
 		<div>
-			<div className="px-8">
+			<div className="pl-4 pr-8">
 				<div>Home</div>
 				<div>Shorts</div>
 				<div>Subscriptions</div>
 			</div>
 			<hr className="my-4" />
-			<div className="px-8">
+			<div className="pl-4 pr-8">
 				<div>Home</div>
 				<div>Shorts</div>
 				<div>Subscriptions</div>
 			</div>
 			<hr className="my-4" />
-			<div className="px-8">
+			<div className="pl-4 pr-8">
 				<div>Home</div>
 				<div>Shorts</div>
 				<div>Subscriptions</div>
