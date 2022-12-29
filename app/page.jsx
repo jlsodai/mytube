5a3a5319f930d13b754f684d1ec9c69b2b15e27b
Jules Maestro <jlsodai@gmail.com>
2022-12-29 16:21:47 +0000

    layout page sections

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import SidebarSection from "../sections/SidebarSection";
+import TopSection from "../sections/TopSection";
+import ContentSection from "../sections/ContentSection";
 
 const Page = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+	return (
+		<div className="wrapper">
+			<TopSection />
+			<div className="grid grid-cols-[240px_1fr]">
+				<SidebarSection />
+				<ContentSection />
+			</div>
+		</div>
+	);
+};
 
-export default Page
+export default Page;
