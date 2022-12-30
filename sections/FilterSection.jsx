2cd40b854c7daa48d476fd06da1bcfc27f1f1f42
Jules Maestro <jlsodai@gmail.com>
2022-12-30 00:12:03 +0000

    create FilterSection — refactor

---
+++ sections/FilterSection.jsx
@@ -0,0 +1,26 @@
+import FilterBtn from "../components/FilterBtn";
+const FilterSection = () => {
+	return (
+		<div className="flex gap-4 flex-nowrap overflow-x-auto filterSection">
+			<FilterBtn title="All" active />
+			<FilterBtn title="Computer programming" />
+			<FilterBtn title="Music" />
+			<FilterBtn title="Mixes" />
+			<FilterBtn title="Live" />
+			<FilterBtn title="Nollywood" />
+			<FilterBtn title="Premier League" />
+			<FilterBtn title="Dramedy" />
+			<FilterBtn title="Podcasts" />
+			<FilterBtn title="Comedy" />
+			<FilterBtn title="Russian Armed Forces" />
+			<FilterBtn title="Contemporary Worship Music" />
+			<FilterBtn title="Children's Music" />
+			<FilterBtn title="Stages" />
+			<FilterBtn title="Pop Music" />
+			<FilterBtn title="Recently Uploaded" />
+			<FilterBtn title="Watched" />
+			<FilterBtn title="New to you" />
+		</div>
+	);
+};
+export default FilterSection;
