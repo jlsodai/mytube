d798e27528ecaf0d1ae5ece1bb30c31fb4d402be
Jules Maestro <jlsodai@gmail.com>
2022-12-30 01:33:34 +0000

    add iconLink hover bg

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -4,8 +4,8 @@ const IconLink = ({ src, href, title, active }) => {
 	return (
 		<Link
 			className={`${
-				active ? "bg-gray-100 rounded-md" : ""
-			} px-3 py-2 flex items-center`}
+				active ? "bg-slate-100" : ""
+			} px-3 py-2 flex items-center hover:bg-slate-100  rounded-md`}
 			href={href ?? "#"}
 		>
 			<Image src={src} alt="" width="24" height="24" />
