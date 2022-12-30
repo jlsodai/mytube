c7160cdc3c8578a1a2830fe8b7701cdfa93d8a0d
Jules Maestro <jlsodai@gmail.com>
2022-12-30 00:28:24 +0000

    further style VideoCard

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 
 const VideoCard = () => {
 	return (
 		<div>
-			<img
-				src="https://source.unsplash.com/rTZW4f02zY8"
-				className="rounded-2xl object-cover h-48 w-full"
-			/>
+			<div className="relative">
+				<img
+					src="https://source.unsplash.com/rTZW4f02zY8"
+					className="rounded-2xl object-cover h-48 w-full"
+					alt=""
+				/>
+				<p className="absolute bottom-3 text-white right-3 bg-black px-2 py-[1px] rounded-lg">
+					20:10
+				</p>
+			</div>
 			<div className="flex mt-3">
 				<img
 					src="https://yt3.ggpht.com/yti/AJo0G0lKS0W4MtyEjHFz-BxmniXDjhsduCoOlnyiZnrySg=s88-c-k-c0x00ffffff-no-rj-mo"
