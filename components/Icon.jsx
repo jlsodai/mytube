c3eeb1acc37556b76c6f20e8fe26bd625530a720
Jules Maestro <jlsodai@gmail.com>
2022-12-30 01:33:15 +0000

    add icon hover bg and radius

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Icon = ({ src }) => {
 	return (
-		<div className="p-3">
+		<div className="p-3 hover:bg-slate-200 hover:rounded-full cursor-pointer">
 			<Image src={src} alt="" width="24" height="24" />
 		</div>
 	);
