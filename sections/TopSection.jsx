ae458ca28c422f6c7f08f5bc355a133e01d952f5
Jules Maestro <jlsodai@gmail.com>
2022-12-30 02:56:14 +0000

    fix responsiveness for search input

--- sections/TopSection.jsx
+++ sections/TopSection.jsx
@@ -4,15 +4,15 @@ import Icon from "../components/Icon";
 
 const TopSection = () => {
 	return (
-		<div className="flex justify-between pl-4 pr-8 py-2 items-center">
+		<div className="flex justify-between pl-4 pr-8 py-2 items-center xl:gap-32">
 			<div className="flex items-center">
 				<Icon src="/svg/hamburger.svg" />
 				<div className="px-4">
 					<Image src="/svg/logo.svg" alt="" width="95" height="24" />
 				</div>
 			</div>
-			<div className="flex items-center gap-4">
-				<div className="relative w-[600px]">
+			<div className="flex items-center gap-4 grow sm:mx-24 max-sm:hidden">
+				<div className="relative w-full">
 					<input
 						type="search"
 						id="search-dropdown"
@@ -31,6 +31,9 @@ const TopSection = () => {
 				<Icon src="/svg/voice-search.svg" />
 			</div>
 			<div className="flex items-center gap-2">
+				<div className="sm:hidden">
+					<Icon src="/svg/search.svg" />
+				</div>
 				<Icon src="/svg/create.svg" />
 				<Icon src="/svg/notifications.svg" />
 				<div className="bg-gray-300 w-[32px] h-[32px] rounded-full overflow-hidden ml-4">
