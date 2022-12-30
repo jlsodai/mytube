bc83b15346057a8d36a05bd0604825d536239d82
Jules Maestro <jlsodai@gmail.com>
2022-12-30 00:12:30 +0000

    add Video Card to content section

--- sections/ContentSection.jsx
+++ sections/ContentSection.jsx
@@ -1,36 +1,19 @@
 import FilterBtn from "../components/FilterBtn";
+import VideoCard from "../components/VideoCard";
+import FilterSection from "./FilterSection";
 const ContentSection = () => {
 	return (
 		<div className="px-8 w-full">
-			<div className="flex gap-4 flex-nowrap overflow-x-auto filterSection">
-				<FilterBtn title="All" active />
-				<FilterBtn title="Computer programming" />
-				<FilterBtn title="Music" />
-				<FilterBtn title="Mixes" />
-				<FilterBtn title="Live" />
-				<FilterBtn title="Nollywood" />
-				<FilterBtn title="Premier League" />
-				<FilterBtn title="Dramedy" />
-				<FilterBtn title="Podcasts" />
-				<FilterBtn title="Comedy" />
-				<FilterBtn title="Russian Armed Forces" />
-				<FilterBtn title="Contemporary Worship Music" />
-				<FilterBtn title="Children's Music" />
-				<FilterBtn title="Stages" />
-				<FilterBtn title="Pop Music" />
-				<FilterBtn title="Recently Uploaded" />
-				<FilterBtn title="Watched" />
-				<FilterBtn title="New to you" />
-			</div>
-			<div className="grid grid-cols-4">
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
-				<div>Video Card</div>
+			<FilterSection />
+			<div className="grid grid-cols-4 mt-8 gap-8">
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
+				<VideoCard />
 			</div>
 			<hr className="my-5" />
 			<div className="grid grid-cols-6">
