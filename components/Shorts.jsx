f48017645164aa2fd0e03009f04fb6ab30eded38
Jules Maestro <jlsodai@gmail.com>
2022-12-30 00:46:16 +0000

    add shorts component

---
+++ components/Shorts.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Shorts = () => {
+	return (
+		<div>
+			<img
+				src="https://source.unsplash.com/rTZW4f02zY8"
+				className="rounded-2xl object-cover h-80 w-full"
+				alt=""
+			/>
+			<div className="mt-3">
+				<h3 className="font-bold">
+					10 ways to use Tailwind CSS and HTML
+				</h3>
+				<p>100K views</p>
+			</div>
+		</div>
+	);
+};
+
+export default Shorts;
