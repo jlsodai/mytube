f943389f24d1b440dc8b3362db2a0851f38bf9f5
Jules Maestro <jlsodai@gmail.com>
2023-01-02 07:11:28 +0000

    add sidebarTopMenu class

--- sections/SidebarTopMenu.jsx
+++ sections/SidebarTopMenu.jsx
@@ -1,7 +1,7 @@
 import IconLink from "../components/IconLink";
 const SidebarTopMenu = () => {
 	return (
-		<>
+		<div className="sidebarTopMenu">
 			<IconLink
 				className="bg-slate-100"
 				src="/svg/home.svg"
@@ -10,7 +10,7 @@ const SidebarTopMenu = () => {
 			/>
 			<IconLink src="/svg/shorts.svg" title="Shorts" />
 			<IconLink src="/svg/subscriptions.svg" title="Subscriptions" />
-		</>
+		</div>
 	);
 };
 export default SidebarTopMenu;
